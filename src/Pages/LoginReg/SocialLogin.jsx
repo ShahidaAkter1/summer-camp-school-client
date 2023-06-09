ffc21b5d1c1d5df5c168af6b2f6cc9feeb419b08
ShahidaAkter1 <shahida0200@gmail.com>
2023-06-09 16:20:06 +0600

    Login Page

---
+++ src/Pages/LoginReg/SocialLogin.jsx
@@ -0,0 +1,50 @@
+import React, { useContext } from 'react';
+import { FaFacebook, FaGoogle } from 'react-icons/fa';
+import { AuthContext } from '../../Provider/AuthProvider';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+
+const SocialLogin = () => {
+
+    const location=useLocation();
+    const navigate=useNavigate();
+    const from=location.state?.from?.pathname || '/';
+
+
+    const { googleSignIn } = useContext(AuthContext);
+    const handleGoogleSignIn = () => {
+        googleSignIn()
+            .then(res => {
+                const user = res.user;
+             navigate(from,{replace:true});
+                // console.log(user);
+                //  navigate(from,{replace:true});
+                toast('Successfully Login With Google!!!')
+
+            })
+            .catch(error => {
+                // console.log(error);
+                toast('  Login failed With Google!!!')
+            })
+    }
+
+
+
+    return (
+        <div className='mx-auto'>
+            <p className='mx-auto' >Or SignUp using  </p>
+            <div className="  mt-6 flex gap-4 mx-auto">
+
+                <button className=" border rounded-full bg-blue-500 p-2 text-white"><FaFacebook className='text-4xl'></FaFacebook></button>
+
+                <button onClick={handleGoogleSignIn} className=" border rounded-full bg-blue-500 p-2 text-white "><FaGoogle className='text-4xl'></FaGoogle></button>
+            </div>
+   <ToastContainer />
+        </div>
+    );
+};
+
+export default SocialLogin;
