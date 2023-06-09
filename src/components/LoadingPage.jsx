f6f910aeb66b6cf3c2ff4f56f8ce45c685bf07f0
ShahidaAkter1 <shahida0200@gmail.com>
2023-06-09 17:07:02 +0600

    Category section added

---
+++ src/components/LoadingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const LoadingPage = () => {
+    return (
+        <div>
+            <div className="badge badge-lg"></div>
+            <div className="badge badge-md"></div>
+            <div className="badge badge-sm"></div>
+            <div className="badge badge-xs"></div>
+        </div>
+    );
+};
+
+export default LoadingPage;
